5a4873da317b48992f528167d80a00be8a2f39bb
Aravinth Rajan <aravinthrajan@outlook.com>
2023-05-31 22:59:56 +0100

    webdriver example

--- src/main/java/selenium/firstSeleniumTestExample.java
+++ src/main/java/selenium/firstSeleniumTestExample.java
@@ -1,5 +1,6 @@
 package selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -21,6 +22,8 @@ public class firstSeleniumTestExample {
 //            System.out.println(driver.getPageSource());
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             Thread.sleep(10000);
+            EnterLoginDetails(driver,"standard_user","secret_sauce");
+            verifyLogin(driver);
 
         } catch (Exception e) {
             System.out.println("Exception occurred" +e);
@@ -37,5 +40,21 @@ public class firstSeleniumTestExample {
     public static void OpenWebPage(WebDriver shalini, String url){
         shalini.get(url);
         shalini.manage().window().maximize();
+    }
+
+    public static void EnterLoginDetails(WebDriver logindriver, String username, String password) throws InterruptedException {
+        logindriver.findElement(By.xpath("//*[@data-test='username']")).sendKeys(username);
+        Thread.sleep(10000);
+        logindriver.findElement(By.xpath("//*[@data-test='password']")).sendKeys(password);
+        Thread.sleep(10000);
+        logindriver.findElement(By.xpath("//*[@data-test=\"login-button\"]")).click();
+        Thread.sleep(10000);
+
+    }
+
+    public static void verifyLogin(WebDriver verifydriver) throws InterruptedException {
+        Thread.sleep(10000);
+        System.out.println("Is cart visible - " + verifydriver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]")).isDisplayed());
+
     }
 }
